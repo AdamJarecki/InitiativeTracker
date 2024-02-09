d88ed43da93a334be4cf036078360875de1c47ce
AdamJarecki <ajarecki37@gmail.com>
2024-02-09 09:54:55 -0700

    Added Set/Get and rollInitiative Functions
    Added setters and getters for all the current variables, and added a rollInitiative function to use random.randint(1,20) plus the self.initiativeBonus to set self.totalInitiative.

--- InitTracker.py
+++ InitTracker.py
@@ -9,11 +9,59 @@
 import random   # Random Number Generator
 
 class Character():
-    def __init__(self, name, initiativeBonus, isPlayer, armorClass):
+    def __init__(self, name, initiativeBonus, isPlayer):
         self.name = name
         self.initiativeBonus = initiativeBonus
         self.isPlayer = isPlayer
-        self.armorClass = armorClass
         self.isIncapacitated = False
         self.totalInitiative = 0
+    
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+    #
+    # Setter and Getter functions
+    # 
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+    def setName(self, name):
+        self.name = name
+        
+    def setInitiativeBonus(self, bonus):
+        self.initiativeBonus = bonus
+        
+    def setIsPlayer(self, isPlayer):
+        self.isPlayer = isPlayer
+        
+    def setIsIncapacitated(self, isIncapacitated):
+        self.isIncapacitated = isIncapacitated
+        
+    def setTotalInitiative(self, totalInitiative):
+        self.totalInitiative = totalInitiative
+
+    def getName(self):
+        return self.name
+    
+    def getInitiativeBonus(self):
+        return self.initiativeBonus
+    
+    def getIsPlayer(self):
+        return self.isPlayer
+    
+    def getIsIncapacitated(self):
+        return self.isIncapacitated
+    
+    def getTotalInitiative(self):
+        return self.totalInitiative
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+    #
+    # Function to roll initiative for a specific character
+    #
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+    def rollInitiative(self):
+        self.totalInitiative = self.initiativeBonus + random.randint(1,20)
+        return self.totalInitiative
+    
+    
+
 
