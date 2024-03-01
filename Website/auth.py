7592fafb0634973a4e2225757ff7155a70aa335f
AdamJarecki <ajarecki37@gmail.com>
2024-03-01 14:34:39 -0700

    fixed Signup Page
    Signup page wasn't updating properly. It was sending everything to the DB no problem, but threw an error on forwarding to the next page due to the login_user() function. I just removed the function.

--- Website/auth.py
+++ Website/auth.py
@@ -56,7 +56,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)            
+            #login_user(user)  # , remember=True         
             flash('Account created!', category='success')
 
             return redirect(url_for('views.home'))
