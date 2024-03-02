dbc8ae31e09c8d39d5a001825aaf9df188e1a6fd
AdamJarecki <ajarecki37@gmail.com>
2024-03-02 15:06:13 -0700

    started the edit-group.html

--- Website/views.py
+++ Website/views.py
@@ -50,3 +50,8 @@ def create_group():
 def initiative_tracker():
     groups = Group.query.filter_by(user_id=current_user.id).all()
     return render_template("initiative-tracker.html", user=current_user)
+
+@views.route('/edit-group.html')
+@login_required
+def edit_group():
+    return render_template("edit-group.html", user=current_user)
