cfc0ea9e0f9dd1b41fe5a05fcb9a44b5701002d8
AdamJarecki <ajarecki37@gmail.com>
2024-03-02 10:34:37 -0700

    included guard statement in views.py

--- Website/views.py
+++ Website/views.py
@@ -17,34 +17,33 @@ def home():
 @views.route('/create-group', methods=['GET', 'POST'])
 @login_required
 def create_group():
-    if request.method == 'POST':
-        group_name = request.form.get('group_name')
-        is_player = bool(int(request.form.get('is_player')))
-        
-        # initiative_bonus = request.form.get('initiative_bonus')
-        
-        # Create a new Group object
-        new_group = Group(group_name=group_name, is_player=is_player, user_id=current_user.id)
-        
-        # Add the group to the database and commit changes
-        db.session.add(new_group)
-        db.session.commit()
-        
-        character_names = request.form.getlist('character_name[]')
-        initiative_bonuses = request.form.getlist('initiative_bonus[]')
-        
-        for name, bonus in zip(character_names, initiative_bonuses):
-            new_character = Character(character_name=name, initiative_bonus=bonus, group_id=new_group.id)
-            db.session.add(new_character)
-            
-        db.session.commit()
-        
-        flash('Group created successfully!', category='success')
-        
-        # Redirect to the initiative tracker page
-        return redirect(url_for('views.initiative_tracker'))
+    if request.method != 'POST':
+        return render_template("create-group.html", user=current_user)
+    group_name = request.form.get('group_name')
+    is_player = bool(int(request.form.get('is_player')))
 
-    return render_template("create-group.html", user=current_user)
+    # initiative_bonus = request.form.get('initiative_bonus')
+
+    # Create a new Group object
+    new_group = Group(group_name=group_name, is_player=is_player, user_id=current_user.id)
+
+    # Add the group to the database and commit changes
+    db.session.add(new_group)
+    db.session.commit()
+
+    character_names = request.form.getlist('character_name[]')
+    initiative_bonuses = request.form.getlist('initiative_bonus[]')
+
+    for name, bonus in zip(character_names, initiative_bonuses):
+        new_character = Character(character_name=name, initiative_bonus=bonus, group_id=new_group.id)
+        db.session.add(new_character)
+
+    db.session.commit()
+
+    flash('Group created successfully!', category='success')
+
+    # Redirect to the initiative tracker page
+    return redirect(url_for('views.initiative_tracker'))
 
 @views.route('/initiative-tracker')
 @login_required
